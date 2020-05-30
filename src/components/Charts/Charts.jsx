d8fa29f4532a30bb3550f8909bd6c4286b91e6d8
kcone09 <kcone09@gmail.com>
2020-05-30 13:27:46 +0200

    corrected bar charts colors

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -47,9 +47,9 @@ const Charts = ({ data: { confirmed, recovered, deaths }, country }) => {
           {
             label: "People",
             backgroundColor: [
-              "rgba(255, 0, 0, 0.5)",
-              "rgba(0, 255, 0, 0.5)",
               "rgba(0, 0, 255, 0.5)",
+              "rgba(0, 255, 0, 0.5)",
+              "rgba(255, 0, 0, 0.5)",
             ],
             data: [confirmed.value, recovered.value, deaths.value],
           },
