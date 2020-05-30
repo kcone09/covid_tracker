6f5a1b5d9297c51677dc9d5054d786311f975467
kcone09 <kcone09@gmail.com>
2020-05-30 13:14:41 +0200

    Updates

---
+++ precache-manifest.863777dc49f930d119939ea695a7b4b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661284ccd66f57b495d04ad6724aae5a",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "76ddd1f5c83064ed94a8",
+    "url": "/covid_tracker/static/css/main.94cb7aeb.chunk.css"
+  },
+  {
+    "revision": "b8548d180a49d4d5d4bc",
+    "url": "/covid_tracker/static/js/2.0d121853.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_tracker/static/js/2.0d121853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ddd1f5c83064ed94a8",
+    "url": "/covid_tracker/static/js/main.c02d778b.chunk.js"
+  },
+  {
+    "revision": "d802bd2ec483788a948f",
+    "url": "/covid_tracker/static/js/runtime-main.0c4458f5.js"
+  },
+  {
+    "revision": "99c305815fc95cb5f89197f1879ce436",
+    "url": "/covid_tracker/static/media/image.99c30581.jpg"
+  }
+]);
